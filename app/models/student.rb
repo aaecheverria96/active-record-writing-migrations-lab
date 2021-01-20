c86677ce5c41a8ae38576b897685acf4a5f2c85a
Your Username <you@example.com>
2021-01-20 15:51:58 -0500

    commit tree 14f6f1d27662fc61f24adc9db6dc91b090c47c04

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,6 @@
-class Student < ActiveRecord::Base
+class Student < ActiveRecord::Base 
+    # def self.create_table 
+    #  sql = "CREATE TABLE students(name TEXT)"
+    #     ActiveRecord::Base.connection.execute(sql) 
+    # end 
 end
